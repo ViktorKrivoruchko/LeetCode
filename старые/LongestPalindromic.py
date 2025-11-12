446286be9d42f44f80d04dca6d41091856a9cf7a
Viktor Krivoruchko <viktor_krivoruchko03@mail.ru>
2025-11-12 22:02:59 +0700

    remove old solutions

--- старые/LongestPalindromic.py
+++
@@ -1,6 +0,0 @@
-# str -> str
-class Solution:
-    def LongestPalindrom(s):
-        new_list = list(s)
-        # reverse_list = new_list.reverse()
-        
--- старые/MedianOfTwo.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def MedianOfTwo(nums1, nums2):
-        num = nums1 + nums2
-        num.sort()
-        if (len(num) % 2) == 0:
-            return((num[len(num) // 2 - 1] + num[len(num) // 2]) / 2)
-        else:
-            return(num[len(num) // 2])
-Solution.MedianOfTwo([1, 3, 5], [5, 6, 2])
