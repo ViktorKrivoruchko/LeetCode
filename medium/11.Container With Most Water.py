410cb37f0d3b2e1d3972f658940a6957d59ff601
Viktor Krivoruchko <viktor_krivoruchko03@mail.ru>
2026-04-17 09:36:05 +0700

    решение 11 задачи

---
+++ medium/11.Container With Most Water.py
@@ -0,0 +1,12 @@
+# O(n) по времени. O(1) по памяти
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        area = 0
+        left, right = 0, len(height)-1
+        while left < right:
+            area = max(area, (right - left) * min(height[left], height[right]))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return area
